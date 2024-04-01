694f55468ebfa29c25003afb369e02b16265ee8c
Joaquim Perez <jivkim@gmail.com>
2024-04-01 18:30:01 +0200

    meta tag to force scale

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $renderedFooter = $converter->convert(file_get_contents('footer.md'));
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>Fête villageoise des Ponts-de-Martel</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
