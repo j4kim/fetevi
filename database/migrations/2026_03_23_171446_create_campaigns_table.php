64d39f118043ec2c757b762330075f73d883e8e8
Joaquim Perez <jivkim@gmail.com>
2026-03-23 18:06:39 +0100

    migration: make name unique

--- database/migrations/2026_03_23_171446_create_campaigns_table.php
+++ database/migrations/2026_03_23_171446_create_campaigns_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("name");
+            $table->string("name")->unique();
             $table->string("title");
             $table->string("description")->nullable();
             $table->string("template");
