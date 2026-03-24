3dd46e1206eb62f6a2e4f425ad1a4037eb23b2e0
Joaquim Perez <jivkim@gmail.com>
2026-03-24 22:17:53 +0100

    remove old 2025 content

--- 2025/header.md
+++
@@ -1,7 +0,0 @@
-# FÊTE VI’
-
-### LES PONTS-DE-MARTEL
-
-## 20-21 JUIN 2025
-
-#### Salle du Bugnon
--- 2025/webhook-target.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$payload = json_decode($_POST['payload']);
-
-if (@$payload->ref == "refs/heads/2025") {
-    echo shell_exec("git pull 2>&1");
-}
