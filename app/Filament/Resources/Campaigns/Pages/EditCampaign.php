30f1b553304c9d01a20749390428708d7d49a067
Joaquim Perez <jivkim@gmail.com>
2026-03-24 22:44:11 +0100

    feat(EditCampaign): ReplicateAction

--- app/Filament/Resources/Campaigns/Pages/EditCampaign.php
+++ app/Filament/Resources/Campaigns/Pages/EditCampaign.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\Campaigns\Pages;
 
 use App\Filament\Resources\Campaigns\CampaignResource;
+use App\Models\Campaign;
 use Filament\Actions\DeleteAction;
+use Filament\Actions\ReplicateAction;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\Width;
 
 class EditCampaign extends EditRecord
 {
@@ -13,6 +17,15 @@ class EditCampaign extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            ReplicateAction::make()
+                ->excludeAttributes(['id'])
+                ->schema([
+                    TextInput::make('name'),
+                ])
+                ->successRedirectUrl(function (Campaign $replica) {
+                    return CampaignResource::getUrl('edit', ['record' => $replica]);
+                })
+                ->modalWidth(Width::Small),
             DeleteAction::make(),
         ];
     }
