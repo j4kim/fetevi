a44b280d631099edfde1b30d5cdbb4d8e2ea6428
Joaquim Perez <jivkim@gmail.com>
2026-03-24 22:16:55 +0100

    web: route to render campaign

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Campaign;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return 'hello';
+});
+
+Route::get('/{campaignName}', function (string $campaignName) {
+    $campaign = Campaign::where('name', $campaignName)->firstOrFail();
+    return view("$campaign->template.index", $campaign);
 });
