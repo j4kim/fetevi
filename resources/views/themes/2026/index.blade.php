f54b03b7aa21b54f5e9893502f6414779fe0c7b4
Joaquim Perez <jivkim@gmail.com>
2026-03-26 19:50:52 +0100

    use tailwind typography for articles

--- resources/views/themes/2026/index.blade.php
+++ resources/views/themes/2026/index.blade.php
@@ -14,16 +14,18 @@
         <header class="bg-[url(fetevi2026-06-3-1--40-notext.webp)] bg-contain text-white aspect-3/1 text-shadow-sm">
             {!! str($header)->markdown()->sanitizeHtml() !!}
         </header>
-        <div class="p-12 flex gap-12 text-[#124d7e] justify-evenly items-start bg-[#f5fcfc]">
+        <div class="p-12 py-20 flex gap-12 text-[#124d7e] justify-evenly items-start bg-[#f5fcfc]">
             @foreach ($articles as $article)
-                <article class="w-1/2 p-6 max-w-lg shadow-lg border-[#3cacc3] rounded-4xl bg-white">
+                <article class="w-1/2 p-6 max-w-xl shadow-lg border-[#3cacc3] rounded-4xl bg-white">
                     <h1>{{ $article['title'] }}</h1>
-                    {!! str($article['content'])->markdown() !!}
+                    <div class="prose lg:prose-lg">
+                        {!! str($article['content'])->markdown() !!}
+                    </div>
                 </article>
             @endforeach
         </div>
     </main>
-    <footer class="flex flex-col items-center gap-4 py-12 bg-linear-to-b from-[#124d7e] to-[#3cacc3] text-white">
+    <footer class="flex flex-col items-center gap-4 py-20 bg-linear-to-b from-[#124d7e] to-[#3cacc3] text-white">
         <img src="svg/logo.svg" class="size-25" />
         {!! str($footer)->markdown() !!}
     </footer>
