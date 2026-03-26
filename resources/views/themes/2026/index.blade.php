4db76c34b2f667452a931a0863689bb2cc7d9da4
Joaquim Perez <jivkim@gmail.com>
2026-03-26 18:44:13 +0100

    wip 2026 theme

--- resources/views/themes/2026/index.blade.php
+++ resources/views/themes/2026/index.blade.php
@@ -5,26 +5,26 @@
 
     <meta property="og:image" content="{{ asset('svg/logo.svg') }}" />
     <meta property="twitter:image" content="{{ asset('svg/logo.svg') }}" />
+
     @vite(['resources/views/themes/2026/app.css', 'resources/views/themes/2026/app.js'])
 @endsection
 
 @section('content')
     <main>
-        <header>
+        <header class="bg-amber-800 text-white aspect-3/1">
             {!! str($header)->markdown()->sanitizeHtml() !!}
         </header>
-        <div class="content">
+        <div class="p-12 flex gap-12 bg-sky-950 justify-evenly items-start">
             @foreach ($articles as $article)
-                <article>
+                <article class="w-1/2 bg-white p-6 max-w-lg">
                     <h1>{{ $article['title'] }}</h1>
                     {!! str($article['content'])->markdown() !!}
                 </article>
             @endforeach
-            <div class="spacer"></div>
         </div>
     </main>
-    <footer>
-        <img src="svg/logo.svg" width="100px" />
+    <footer class="flex flex-col items-center gap-4 my-12">
+        <img src="svg/logo.svg" class="size-25" />
         {!! str($footer)->markdown() !!}
     </footer>
 @endsection
