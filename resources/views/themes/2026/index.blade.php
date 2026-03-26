afec1ac360e7c3e268ed34cc9db8d5c8ba8f589e
Joaquim Perez <jivkim@gmail.com>
2026-03-26 19:01:43 +0100

    wip 2026 theme

--- resources/views/themes/2026/index.blade.php
+++ resources/views/themes/2026/index.blade.php
@@ -14,16 +14,16 @@
         <header class="bg-[url(fetevi2026-06-3-1--40-notext.webp)] bg-contain text-white aspect-3/1">
             {!! str($header)->markdown()->sanitizeHtml() !!}
         </header>
-        <div class="p-12 flex gap-12 bg-sky-950 justify-evenly items-start">
+        <div class="p-12 flex gap-12 bg-white text-[#124d7e] justify-evenly items-start">
             @foreach ($articles as $article)
-                <article class="w-1/2 bg-white p-6 max-w-lg">
+                <article class="w-1/2 p-6 max-w-lg">
                     <h1>{{ $article['title'] }}</h1>
                     {!! str($article['content'])->markdown() !!}
                 </article>
             @endforeach
         </div>
     </main>
-    <footer class="flex flex-col items-center gap-4 my-12">
+    <footer class="flex flex-col items-center gap-4 py-12 bg-linear-to-b from-[#124d7e] to-[#3cacc3] text-white">
         <img src="svg/logo.svg" class="size-25" />
         {!! str($footer)->markdown() !!}
     </footer>
