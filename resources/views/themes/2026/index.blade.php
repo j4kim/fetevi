b62b62f7c23628618a2edcd0f7572bde0673b739
Joaquim Perez <jivkim@gmail.com>
2026-03-26 22:44:26 +0100

    format to reorder tailwind classes

--- resources/views/themes/2026/index.blade.php
+++ resources/views/themes/2026/index.blade.php
@@ -12,13 +12,13 @@
 @section('content')
     <main>
         <header
-            class="bg-[url(fetevi2026-06-1-1--40-notext.webp)] bg-contain text-white aspect-square  text-shadow-sm md:aspect-3/1 md:bg-[url(fetevi2026-06-3-1--40-notext.webp)]">
+            class="text-shadow-sm md:aspect-3/1 aspect-square bg-[url(fetevi2026-06-1-1--40-notext.webp)] bg-contain text-white md:bg-[url(fetevi2026-06-3-1--40-notext.webp)]">
             {!! str($header)->markdown()->sanitizeHtml() !!}
         </header>
         <div
-            class="p-6 md:p-12 py-16 md:py-20 xl:py-28 flex-col flex md:flex-row gap-12 text-[#124d7e] justify-evenly items-start bg-[#f5fcfc]">
+            class="flex flex-col items-start justify-evenly gap-12 bg-[#f5fcfc] p-6 py-16 text-[#124d7e] md:flex-row md:p-12 md:py-20 xl:py-28">
             @foreach ($articles as $article)
-                <article class="w-full md:w-1/2 p-4 md:p-6 md:max-w-xl shadow-lg border-[#3cacc3] rounded-4xl bg-white">
+                <article class="rounded-4xl w-full border-[#3cacc3] bg-white p-4 shadow-lg md:w-1/2 md:max-w-xl md:p-6">
                     <h1 class="text-3xl xl:text-5xl">{{ $article['title'] }}</h1>
                     <div class="prose lg:prose-lg xl:prose-xl">
                         {!! str($article['content'])->markdown() !!}
@@ -27,7 +27,7 @@
             @endforeach
         </div>
     </main>
-    <footer class="flex flex-col items-center gap-4 py-20 bg-linear-to-b from-[#124d7e] to-[#3cacc3] text-white">
+    <footer class="bg-linear-to-b flex flex-col items-center gap-4 from-[#124d7e] to-[#3cacc3] py-20 text-white">
         <img src="svg/logo.svg" class="size-25" />
         {!! str($footer)->markdown() !!}
     </footer>
